2594b8d7b8cf569fe09076beadadac52d1d561d1
FrancoFelg <franco_lamberti_02@yahoo.com.ar>
2022-12-05 12:39:17 -0300

    Dynamic List completed

---
+++ MVC004/Datos/Conexion.cs
@@ -0,0 +1,24 @@
+﻿namespace MVC004.Datos
+{
+    public class Conexion
+    {
+        private string cadenaSQL = string.Empty;
+        public Conexion()
+        {
+            //El builder crea todo lo necesario para conectarse a la base de datos
+            var builder =
+                new ConfigurationBuilder().SetBasePath(
+                Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+
+            //Almacena la cadena del builder
+            cadenaSQL = builder.GetSection("ConnectionStrings:CadenaSQL").Value;
+
+        }
+
+        public string getCadenaSQL()
+        {
+            return cadenaSQL;
+        }
+    }
+}
