a953a9bafd6284a4560131de87264afb4b3064e7
FrancoFelg <franco_lamberti_02@yahoo.com.ar>
2022-12-12 21:52:46 -0300

    Product CRUD

--- MVC004/Controllers/ProductController.cs
+++ MVC004/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC004.Models;
 using MVC004.Datos;
 
 namespace MVC004.Controllers
@@ -8,8 +9,67 @@ namespace MVC004.Controllers
         ProductoDatos productoDatos = new ProductoDatos();
         public IActionResult Index()
         {
+            var producto = new Producto();
+            return View(producto);
+        }
+
+        [HttpPost]
+        public IActionResult Guardar(Producto producto)
+        {
+            //System.Diagnostics.Debug.WriteLine("La respuesta fue : " + producto.id + producto.nombre );
+            var respuesta = productoDatos.save(producto);
+
+            if (respuesta)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public IActionResult Editar(int id)
+        {
+            var producto = productoDatos.getById(id);
+            return View(producto);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Producto producto)
+        {
+            
+            var respuesta = productoDatos.edit(producto);
+            System.Diagnostics.Debug.WriteLine("UPD_" + respuesta);
+            if (respuesta)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Tabla()
+        {
             var objLista = productoDatos.getAllProducts();
             return View(objLista);
         }
+
+        public IActionResult Eliminar(int id)
+        {
+            var respuesta = productoDatos.deleteById(id);
+            if (respuesta)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
     }
 }
--- MVC004/Models/Producto.cs
+++ MVC004/Models/Producto.cs
@@ -2,9 +2,8 @@
 {
     public class Producto
     {
-        public int id { get; set; }
-        public string? nombre { get; set; }
-        public int? promocion_id { get; set; }
+        public int? id { get; set; }
+        public string? nombre { get; set; }        
 
     }
 }
