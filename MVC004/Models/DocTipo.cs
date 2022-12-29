97fd95cab85b702298a4f3ae988cffefde806793
FrancoFelg <franco_lamberti_02@yahoo.com.ar>
2022-12-28 21:07:38 -0300

    Entregar Proyecto Final

--- MVC004/Models/DocTipo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MVC004.Models
-{
-    public class DocTipo
-    {
-        public int? id { get; set; }
-        public string? doc_tipo { get; set; }
-    }
-}
--- MVC004/Models/Producto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MVC004.Models
-{
-    public class Producto
-    {
-        public int? id { get; set; }
-        public string? nombre { get; set; }        
-
-    }
-}
